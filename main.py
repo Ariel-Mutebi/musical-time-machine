cc8ee3500c0ef4fa8167e725f012fc6eded7f765
black-RAM <pplanes311@gmail.com>
2025-06-16 20:43:42 +0300

    I tried out authentication with Spotify (and it worked)!

--- main.py
+++ main.py
@@ -3,15 +3,33 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
+REDIRECT_URI = "https://ariel-mutebi.github.io/musical-time-machine/"
 
 # Scrape Billboard Hot 100
-date_string = input("What day do you want to musically travel back to? Type the date in the format YYYY-MM-DD: ")
-headers = {"User-Agent": os.environ.get("USER_AGENT")}
-response = requests.get(f"https://www.billboard.com/charts/hot-100/{date_string}", headers=headers)
-response.raise_for_status()
-let_him_cook = BeautifulSoup(response.text, "html.parser")
-song_titles = [re.sub(r'[\n\t]', '', h3.getText()) for h3 in let_him_cook.select("li>h3#title-of-a-story")]
-print(song_titles)
+# date_string = input("What day do you want to musically travel back to? Type the date in the format YYYY-MM-DD: ")
+# headers = {"User-Agent": os.environ.get("USER_AGENT")}
+# response = requests.get(f"https://www.billboard.com/charts/hot-100/{date_string}", headers=headers)
+# response.raise_for_status()
+# let_him_cook = BeautifulSoup(response.text, "html.parser")
+# song_titles = [re.sub(r'[\n\t]', '', h3.getText()) for h3 in let_him_cook.select("li>h3#title-of-a-story")]
+# print(song_titles)
 
+# Test Spotify authentication by getting my liked songs
+spotify = Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=os.environ.get("SPOTIFY_CLIENT_ID"),
+        client_secret=os.environ.get("SPOTIFY_CLIENT_SECRET"),
+        redirect_uri=REDIRECT_URI,
+        scope="user-library-read"
+    )
+)
+
+my_liked_songs = spotify.current_user_saved_tracks()
+
+for index, item in enumerate(my_liked_songs["items"], start=1):
+    track = item["track"]
+    print(f"{index}. {track['name']} by {track['artists'][0]['name']}")
